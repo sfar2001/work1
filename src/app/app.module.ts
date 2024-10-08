f67f3257431d6cfafd27516ceb0c0614d29056d9
[adam sfar] <[sfar.adam@esprit.tn]>
2024-10-08 14:29:12 +0100

    test angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { ListCategoriesComponentComponent } from './list-categories-component/list-categories-component.component';
+import { MyPipePipe } from './my-pipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    FooterComponentComponent,
+    HomeComponentComponent,
+    ListCategoriesComponentComponent,
+    MyPipePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/list-categories-component/list-categories-component.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Categorie } from 'src/models/Categorie';
+@Component({
+  selector: 'app-list-categories-component',
+  templateUrl: './list-categories-component.component.html',
+  styleUrls: ['./list-categories-component.component.css']
+})
+export class ListCategoriesComponentComponent {
+category : Categorie[]= [{"id":1,"title":"Grand électroménager",
+"image":"assets/images/categorie_electromenager.jpg", "description":"bon etat id1",
+"available":true},
+{"id":2,"title":"Petit électroménager",
+"image":"assets/images/categorie_petit_electromenager.jpg", "description":"bon etat id2",
+"available":true},
+{"id":3,"title":"Produits informatiques",
+"image":"assets/images/categorie_produits_informatiques.jpg", "description":"bon etat id3",
+"available":true},
+{"id":4,"title":"Smart Phones", "image":"assets/images/categorie_smartPhone.jpg",
+"description":"bon etat id4", "available":true},
+{"id":5,"title":"TV, images et son",
+"image":"assets/images/categorie_tv_image_son.jpg", "description":"bon etat id5",
+"available":true},
+{"id":6,"title":"Produits voiture", "image":"assets/images/produits_nettoyages.jpg",
+"description":"bon etat id6","available":false},
+];
+func1(any:string){
+    console.log(any);
+    alert(any);
+}
+}
